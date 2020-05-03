8a5f0bfa66fd852ba23bddaedb4770d749e6768f
Omar Ong <44765030+jetsetpanda@users.noreply.github.com>
2020-05-03 02:45:00 -0400

    api linked to fe
    Signed-off-by: jetsetPanda <marionong@gmail.com>

--- app.js
+++ app.js
@@ -21,4 +21,11 @@ app.use('/graphql', graphqlHttp({
     graphiql: true
 }));
 
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@upgalactic-b8cic.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`).then(() => { app.listen(3000); }).catch(err => { console.log(err); });
+mongoose
+    .connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@upgalactic-b8cic.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`)
+    .then(() => {
+        app.listen(8000);
+    })
+    .catch(err => {
+        console.log(err);
+    });
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,11 +1,69 @@
 import React, {Component} from 'react';
 
+import './Login.css'
+
 class LoginPage extends Component {
+    constructor(props) {
+        super(props);
+        this.emailElemFX = React.createRef();
+        this.passwordElemFX = React.createRef();
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        const email = this.emailElemFX.current.value;
+        const password = this.passwordElemFX.current.value;
+
+        if (email.trim().length === 0 || password.trim().length === 0) {
+            return;
+        }
+
+        console.log(email,password);
+
+        const requestBody = {
+            query: `
+                mutation{
+                    createUser(userArg: {
+                        email: "${email}",
+                        password: "${password}"
+                    }) {
+                        _id
+                        email
+                    }
+                }
+            `,
+        };
+
+        fetch('http://localhost:8000/graphql', {
+            method: 'POST', //cuz graphQL ;)
+            body: JSON.stringify(requestBody),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+    };
+
     render() {
         return (
-            <div>
+            <form className="login-form" onSubmit={this.handleSubmit}>
                 <h1 style={{color: "#75d11a"}}>Sign In</h1>
-            </div>
+                <div className="form-item">
+                    <label htmlFor="email">E-Mail</label>
+                    <input type="email" id="email" ref={this.emailElemFX}/>
+                </div>
+                <div className="form-item">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" ref={this.passwordElemFX}/>
+                </div>
+                <div className="form-action">
+                    <button type="submit">Submit</button>
+                    <button type="button">Sign Up</button>
+
+
+                </div>
+
+            </form>
         );
     }
 }
