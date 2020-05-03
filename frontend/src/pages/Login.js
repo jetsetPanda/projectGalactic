4abc68dc2145b3c7335818e7b5f455e2cdd3a47a
Omar Ong <44765030+jetsetpanda@users.noreply.github.com>
2020-05-03 02:05:38 -0400

    core frontend
    Signed-off-by: jetsetPanda <marionong@gmail.com>

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class LoginPage extends Component {
+    render() {
+        return (
+            <div>
+                <h1 style={{color: "#75d11a"}}>Sign In</h1>
+            </div>
+        );
+    }
+}
+
+export default LoginPage;
