dbc94a57850e9703c0629a5855e116500540e46c
jetsetPanda <44765030+jetsetPanda@users.noreply.github.com>
2020-05-07 18:59:15 -0400

    switch cleanup route switch clnup
    Signed-off-by: jetsetPanda <44765030+jetsetPanda@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -38,10 +38,10 @@ class App extends Component {
                         <MainNavbar/>
                         <main className='main'>
                             <Switch>
-                                {!this.state.token && <Redirect from="/" to="/auth" exact />}
-                                {!this.state.token && <Redirect from="/bookings" to="/auth" exact />}
                                 {this.state.token && <Redirect from="/" to="/events" exact />}
-                                {this.state.token && <Redirect from="/auth" to="/events" exact />}
+                                {this.state.token && (
+                                    <Redirect from="/auth" to="/events" exact />
+                                )}
                                 {!this.state.token && (
                                     <Route path="/auth" component={LoginPage} />
                                 )}
@@ -49,6 +49,7 @@ class App extends Component {
                                 {this.state.token && (
                                     <Route path="/bookings" component={BookingsPage} />
                                 )}
+                                {!this.state.token && <Redirect to="/auth" exact />}
                             </Switch>
                         </main>
                     </AuthContext.Provider>
