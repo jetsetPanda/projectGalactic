45dac1b03dbb164f73875d070f2b0964584667d0
jetsetPanda <44765030+jetsetPanda@users.noreply.github.com>
2020-05-07 19:18:11 -0400

    modal base
    Signed-off-by: jetsetPanda <44765030+jetsetPanda@users.noreply.github.com>

---
+++ frontend/src/components/Modal/Modal.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Modal.css';
+
+const Modal = props => (
+    <div className='modal'>
+        <header>
+            {props.title}
+        </header>
+        <section className="modal-content">
+            {props.children}
+        </section>
+        <section className="modal-actions">
+            {props.canCanel && <button className='btn'>Cancel</button>}
+            {props.canConfirm && <button className='btn'>Confirm</button>}
+        </section>
+    </div>
+);
+
+export default Modal;
--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,11 +1,22 @@
 import React, {Component} from 'react';
 
+import Modal from "../components/Modal/Modal";
+import './Events.css';
+
 class EventsPage extends Component {
     render() {
         return (
-            <div>
-                <h1>Events Page</h1>
-            </div>
+            <React.Fragment>
+                <Modal>
+                    Modals
+                </Modal>
+                <h1>Events Control Panel</h1>
+                <div className='events-control'>
+                    <p>Design an Event</p>
+                    <button className='btn'>Create Event</button>
+                </div>
+            </React.Fragment>
+
         );
     }
 }
