08f2c490f84652aec3859ad8f50fd94359ca9494
jetsetPanda <44765030+jetsetPanda@users.noreply.github.com>
2020-05-12 01:09:20 -0400

    batching api requests
    Signed-off-by: jetsetPanda <44765030+jetsetPanda@users.noreply.github.com>

--- graphql/resolvers/getters.js
+++ graphql/resolvers/getters.js
@@ -1,6 +1,18 @@
+//mergers --
+
+const DataLoader = require('dataloader');
+
 const EventMGS = require('../../models/event');
 const UserMGS = require('../../models/user');
 const { dateHelper } = require('../../helpers/index');
+
+const eventLoader = new DataLoader((eventIds) => {
+    return getEvents(eventIds);
+});
+
+const userLoader = new DataLoader((userIds) => {
+    return  UserMGS.find({ _id: { $in: userIds } });
+});
 
 const transformEvent = eventObj => {
     return {
@@ -11,10 +23,20 @@ const transformEvent = eventObj => {
         creator: getUser.bind(this, eventObj.creator)
     };
 };
+//
+// const transformUser = userObj => {
+//     return {
+//         ...user._doc,
+//         _id: user.id,
+//         password: "hashed", //ako ra remove
+//         //below: summons function upon graphQL query
+//         createdEvents: eventLoader.loadMany.bind(this, user._doc.createdEvents)
+//     };
+// }
 
-const getEventsList = async eventIdList => { //args: array
+const getEvents = async eventIds => { //args: array
     try {
-        const events = await EventMGS.find({ _id: { $in: eventIdList } }); //mgse query to look inside args arr
+        const events = await EventMGS.find({ _id: { $in: eventIds } }); //mgse query to look inside args arr
         return events.map(event => {
             return transformEvent(event);
         });
@@ -23,24 +45,34 @@ const getEventsList = async eventIdList => { //args: array
     }
 };
 
-const getEvent = async eventId => {
+const getSingleEvent = async eventId => {
     try {
-        const event = await EventMGS.findById(eventId);
-        return transformEvent(event);
+        return await eventLoader.load(eventId.toString());
     } catch (err) {
         throw err;
     }
 };
 
+// const getUsers = async userIds => {
+//     try {
+//         const users = await UserMGS.find({ _id: { $in: userId } });
+//         return users.map(user => {
+//             return transformUser(user);
+//         });
+//     } catch (err) {
+//         throw err;
+//     }
+// };
+
 const getUser = async userId => {
     try {
-        const user = await UserMGS.findById(userId);
+        const user = await userLoader.load(userId.toString()); //invoke toStr to bypass proto type error // ids are objs in mdb
         return {
             ...user._doc,
             _id: user.id,
             password: "hashed", //ako ra remove
             //below: summons function upon graphQL query
-            createdEvents: getEventsList.bind(this, user._doc.createdEvents)
+            createdEvents: eventLoader.loadMany.bind(this, user._doc.createdEvents)
         };
     } catch (err) {
         throw err;
@@ -48,6 +80,6 @@ const getUser = async userId => {
 };
 
 // exports.getEventsList = getEventsList;
-exports.getEvent = getEvent;
+exports.getEvent = getSingleEvent;
 exports.getUser = getUser;
 exports.transformEvent = transformEvent;
